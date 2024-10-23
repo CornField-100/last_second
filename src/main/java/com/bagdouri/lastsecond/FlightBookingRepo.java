d3b3fc3f83710355a1d0412aa7498cf9bb8903df
kiend <quangdang271105@gmail.com>
2024-10-23 13:19:15 +0200

    Fixing

--- src/main/java/com/bagdouri/lastsecond/FlightBookingRepo.java
+++
@@ -1,6 +0,0 @@
-package com.bagdouri.lastsecond;
-import com.bagdouri.lastsecond.FlightBooking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightBookingRepo extends JpaRepository<FlightBooking, Long> {
-}
---
+++ src/main/java/com/bagdouri/lastsecond/FlightBookingRepository.java
@@ -0,0 +1,5 @@
+package com.bagdouri.lastsecond;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightBookingRepository extends JpaRepository<FlightBooking, Long> {
+}
