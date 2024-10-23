851b0217e5cee646c41ad3af5e1bc055998263ea
kiend <quangdang271105@gmail.com>
2024-10-23 10:57:52 +0200

    Fixing

---
+++ src/main/java/com/bagdouri/lastsecond/error/LocalControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.bagdouri.lastsecond.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class LocalControllerAdvice {
+    //Global exceptions can be added here
+}
