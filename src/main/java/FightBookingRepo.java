afeaa7d9a4d2c5b5f69dfa2fc2f6169540dff506
kiend <quangdang271105@gmail.com>
2024-10-23 13:16:42 +0200

    Fixing

--- src/main/java/FightBookingRepo.java
+++
@@ -1,5 +0,0 @@
-import com.bagdouri.lastsecond.FlightBooking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FightBookingRepo extends JpaRepository<FlightBooking, Long> {
-}
